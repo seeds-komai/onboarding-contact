7ba39beb0ed2a44d362116414059e2e5421c4773
{s.komai} <{s.komai@seeds-std.co.jp}>
2024-09-11 16:26:30 +0900

    外部キーの設定が間違っていたため変更

--- docker/db/init_data/002_CREATE_TABLE.sql
+++ docker/db/init_data/002_CREATE_TABLE.sql
@@ -2,5 +2,5 @@
 CREATE TABLE IF NOT EXISTS reasons(
     contact_id BIGINT,
     reason VARCHAR(255),
-    FOREIGN KEY(id) REFERENCES contacts(id) ON DELETE CASCADE
+    FOREIGN KEY(contact_id) REFERENCES contacts(id) ON DELETE CASCADE
 );
